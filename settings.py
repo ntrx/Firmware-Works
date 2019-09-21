12d528319cec3d5f41762ce4cd779a07eb3e8265
ntrx <55629926+ntrx@users.noreply.github.com>
2019-09-21 18:53:25 +0300

    Create settings.py
    example version

---
+++ settings.py
@@ -0,0 +1,13 @@
+# Auto created settings file. Remember that value is framed by '' 
+user = 'root' # user login to device (root) 
+host = '192.168.9.200' # device IP 
+secret = '' # user pass to device (empty) 
+project = 'sn4215' # project name (sn4215, sn3307) 
+source = 'D:\halashchenko\SN4215N' # path to project (must contain: Build, Src) 
+global_build_server = '192.168.9.111' # Build-Server IP  
+global_bs_user = 'ntrx' # Your login to build-server 
+global_bs_secret = '1111' # Pass
+update = '1' #  0 - update, 1 - sync 
+ftp_mode = '1' # - 1 - use winSCP, 0 - paramiko  
+path_scp = 'C:/Program Files (x86)/WinSCP/WinSCP.com' # - path to WinSCP .com file 
+path_putty = 'C:/Program Files/PuTTY/putty.exe' # - path to Putty exe file 
