aafb538808c9f7c366b3d4d87643c34b37440f41
ntrx <ntrxhm@gmail.com>
2019-10-04 13:44:09 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,13 +1,14 @@
 # Auto created settings file. Remember that value is framed by '' 
-user = 'root' # user login to device (root) 
+user = 'root' # user login to device (default: root) 
 host = '192.168.9.200' # device IP 
-secret = '' # user pass to device (empty) 
+secret = '' # user pass to device (default: empty) 
 project = 'sn4215' # project name (sn4215, sn3307) 
-source = 'D:\halashchenko\SN4215N' # path to project (must contain: Build, Src) 
+source = 'D:\SN4215N' # path to project (must contain: Build, Src)
 global_build_server = '192.168.9.111' # Build-Server IP  
-global_bs_user = 'ntrx' # Your login to build-server 
+global_bs_user = 'user' # Your login to build-server
 global_bs_secret = '1111' # Pass
-update = '1' #  0 - update, 1 - sync 
+update = '0' #  0 - update, 1 - sync 
 ftp_mode = '1' # - 1 - use winSCP, 0 - paramiko  
 path_scp = 'C:/Program Files (x86)/WinSCP/WinSCP.com' # - path to WinSCP .com file 
 path_putty = 'C:/Program Files/PuTTY/putty.exe' # - path to Putty exe file 
+path_psplash = '' # - path to pslpash file
